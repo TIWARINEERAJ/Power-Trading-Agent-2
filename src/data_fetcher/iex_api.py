e76cc7ba3da1efb04420e238ad9681b4a9d86076
TIWARINEERAJ <neeraj28tiwari@gmail.com>
2025-05-10 21:54:27 +0530

    Fix Selenium WebDriver init: use Service for ChromeDriver path

--- src/data_fetcher/iex_api.py
+++ src/data_fetcher/iex_api.py
@@ -5,6 +5,7 @@ from loguru import logger
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
@@ -20,7 +21,8 @@ def get_chrome_driver():
     chrome_options.add_argument('--disable-extensions')
     chrome_options.add_argument('--remote-debugging-port=9222')
     chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
-    return webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    service = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=service, options=chrome_options)
 
 class IEXScraper:
     def __init__(self):
